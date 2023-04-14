54d032a1a1fa92b01c23840d7b441789bc92d62d
Алексей Барейша <aleksejbarejsa@MacBook-Air-Aleksej.local>
2023-04-14 23:06:03 +0300

    reviews/models.py - сделал модели

---
+++ reviews/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Модель Категорий"""
+
+    name = models.CharField(
+        max_length=256,
+        unique=True,
+        verbose_name='Название категории',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Псевдоним категории',
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        """Возвращает slug категории."""
+        return self.slug
+
+
+class Genre(models.Model):
+    """Модель Жанра"""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра',
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Псевдоним жанра',
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        """Возвращает slug жанра."""
+        return self.slug
+
+
+class Title(models.Model):
+    """Модель Произведения"""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения',
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год выпуска',
+    )
+    description = models.TextField(
+        verbose_name='Описание произведения',
+        null=True,
+        blank=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория произведения',
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        default_related_name = 'titles'
+
+    def __str__(self):
+        """Возвращает название произведения."""
+        return self.name
