2e7af5d04c13e1219e421d66afed09b796cc3dcb
Rezuce <lisvladimir148@yandex.ru>
2023-04-15 20:55:32 +0300

    Сделал доп поле для User

--- api_yamdb/User/models.py
+++ api_yamdb/User/models.py
@@ -2,3 +2,8 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
+class User(AbstractUser):
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True,
+    )
